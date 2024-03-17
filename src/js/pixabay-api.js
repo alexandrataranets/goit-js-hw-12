1779f9c635c9a103b0cab4d7e5a5eba82e14950f
alexandrataranets <142220751+alexandrataranets@users.noreply.github.com>
2024-03-17 08:57:38 +0100

    update css, index.html, create folder js

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_KEY = '42710952-cb07850fe6c5f6774b64d780f';
+const baseURL = 'https://pixabay.com/api/';
+
+export async function fetchImages(searchQuery, page, perPage) {
+  try {
+    const response = await axios.get(baseURL, {
+      params: {
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: perPage,
+      },
+    });
+
+    return response.data.hits;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,145 @@
+import { fetchImages } from './js/pixabay-api';
+import './css/loader-styles.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import {
+  renderGallery,
+  galleryElement,
+  showEndOfCollectionMessage,
+  appendImagesToGallery,
+} from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const scrollToTopBtn = document.querySelector('.scroll-to-top');
+
+const searchForm = document.querySelector('.form');
+const inputElement = document.querySelector('.search-input');
+const loader = document.querySelector('.loader');
+const loadMoreBtn = document.querySelector('.load-more-btn');
+
+hideLoader();
+
+let searchTerm = '';
+let pageCounter = 1;
+const perPage = 15;
+let totalHits = 0;
+
+searchForm.addEventListener('submit', submitHandle);
+async function submitHandle(event) {
+  event.preventDefault();
+  searchTerm = inputElement.value.trim();
+  pageCounter = 1;
+
+  if (searchTerm === '') {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a search term.',
+      position: 'topCenter',
+    });
+    hideLoadMoreBtn();
+
+    return;
+  }
+
+  hideEndOfCollectionMessage();
+
+  showLoader();
+  try {
+    const images = await fetchImages(searchTerm, pageCounter, perPage);
+
+    if (images.length === 0) {
+      galleryElement.innerHTML = '';
+      iziToast.info({
+        title: 'Info',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topCenter',
+      });
+      hideLoadMoreBtn();
+      return;
+    } else {
+      renderGallery(images);
+      showLoadMoreBtn();
+      inputElement.value = '';
+    }
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to fetch images. Please try again later.',
+      position: 'topCenter',
+    });
+  } finally {
+    hideLoader();
+  }
+}
+
+loadMoreBtn.addEventListener('click', async () => {
+  try {
+    showLoader();
+    const images = await fetchImages(searchTerm, (pageCounter += 1), perPage);
+    totalHits = images.totalHits;
+    if (images.length < perPage || images.length >= totalHits) {
+      hideLoadMoreBtn();
+      showEndOfCollectionMessage();
+    }
+
+    appendImagesToGallery(images);
+
+    const galleryCardHeight =
+      galleryElement.firstElementChild.getBoundingClientRect().height;
+    window.scrollBy({ top: galleryCardHeight * 3, behavior: 'smooth' });
+  } catch (error) {
+    console.error('Error fetching more images:', error);
+    iziToast.error({
+      title: 'Error',
+      message: `Error fetching more images: ${error}`,
+    });
+  } finally {
+    hideLoader();
+  }
+});
+
+// *loader
+function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+function hideLoader() {
+  loader.classList.add('hidden');
+}
+
+// * button load more images
+function showLoadMoreBtn() {
+  loadMoreBtn.style.display = 'block';
+}
+
+function hideLoadMoreBtn() {
+  loadMoreBtn.style.display = 'none';
+}
+
+function hideEndOfCollectionMessage() {
+  const endMessage = document.querySelector('.end-message');
+  if (endMessage) {
+    endMessage.remove();
+  }
+}
+
+// * scroll
+window.addEventListener('scroll', () => {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    scrollToTopBtn.style.display = 'flex';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+scrollToTopBtn.addEventListener('click', scrollToTop);
+// console.log(response.data.totalHits);
